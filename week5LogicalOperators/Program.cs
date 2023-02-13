08c7d2f3034cfc179f3e288c619021af771c5127
Marleen Mesults <marleen.mesults@tktk.ee>
2023-02-13 18:58:53 +0200

    add week5logicaloperators code

---
+++ week5LogicalOperators/Program.cs
@@ -0,0 +1,11 @@
+﻿
+//AND - &&
+
+//true && true && true --> true
+//true && false && true --> false
+
+//OR - || (pipes=
+
+//true || true --> true
+//true || flase --> true
+//false || false --> false
